902e72b4c9c69c1acca879cbe0fde7bf39315cd9
Raj Bharti <itsraj@Rajs-MacBook-Air-2.local>
2025-01-25 14:53:39 +0530

    swallow copy and deep copy notes

--- shallow_deep_copy.js
+++ shallow_deep_copy.js
@@ -1,1 +1,39 @@
-#shallowcopy #deepcopy
+
+// let a={
+//     b:'raj',
+//     c:{
+//         d:'jimmy'
+//     }
+// }
+const lodash = require('lodash');
+// import lodash from ''
+// let d={...a}
+// // let d=a
+// // shallow copy in which only first level attribute is getting copied
+// // let d=Object.assign({},a)
+// d.b='himanshu'
+// console.log(a);
+// console.log(d);
+
+//deep copy 
+
+// let a={
+//     b:'raj',
+//     c:undefined
+// }
+// // let d=JSON.parse(JSON.stringify(a)) // deep copy but data loss
+// let d=structuredClone(a)  // deep copy but no data loss (when there is non-serizable property )
+// // let d={...a} // shallow copy 
+// console.log(d)
+// console.log(a)
+
+let a1={
+   value:()=>{
+      return 6
+   }
+}
+// let a=JSON.parse(JSON.stringify(a1))
+// let a=structuredClone(a1)
+  let a=lodash.cloneDeep(a1) // best way if therer is function 
+console.log(a)
+
